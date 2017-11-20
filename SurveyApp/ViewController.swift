ef4dedc4b2bedc7fc32ee738f62f230367df7db2
nesreenM <nesreen.mouti@gmail.com>
2017-11-20 12:48:39 +0200

    Initial Commit

---
+++ SurveyApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SurveyApp
+//
+//  Created by Nesreen Mamdouh on 11/20/17.
+//  Copyright © 2017 JRany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
